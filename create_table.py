54130ebbb6655e2d8eef6a922e886200db6d619e
Surya Kumar V <71368792+SuryaKumar-IoT@users.noreply.github.com>
2020-11-04 13:57:22 +0530

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -1,12 +1,5 @@
 import boto3
-
-#dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-dynamodb=boto3.client(
-    "dynamodb",
-    aws_access_key_id="AKIAXHI5XG5NM4OBVWYK",
-    aws_secret_access_key="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",
-    region_name="us-east-1"
-)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.create_table(
     TableName='PhoneBook',
